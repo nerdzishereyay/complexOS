e756be80f7d3187249ef004479be31b2c5384ba3
nerdz <nerdzfarch@gmail.com>
2026-05-14 18:44:00 -0500

    Update complexwalletmgr.cpp

--- tools/complexwalletmgr.cpp
+++ tools/complexwalletmgr.cpp
@@ -5,6 +5,7 @@ using namespace std;
 int main () {
   cout << endl << "Launched cOS Wallet Manager" << endl;
   cout << "\n Version 1.0 \n";
+  cout << "THIS IS A NEW TEST TO SEE IF MY CODE AUTO UPDATES";
   return 0;
 }
 
