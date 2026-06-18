9dfcc14e75b27475835a84cdc57dadc719a198d2
nerdz <nerdzfarch@gmail.com>
2026-06-18 16:36:12 -0500

    Update complexwalletmgr.cpp

--- tools/complexwalletmgr.cpp
+++ tools/complexwalletmgr.cpp
@@ -1,11 +1,46 @@
+// Wallet manager, stores passwords/valuable information
 
+
+
+#include <chrono>
 #include <iostream>
-using namespace std;
+#include <list>
+#include <thread>
+#include <utility>
+#include <vector>
+#include "ftxui/component/screen_interactive.hpp"
+#include "ftxui/component/captured_mouse.hpp"  // for ftxui
+#include "ftxui/component/component.hpp"  // for Button, Horizontal, Renderer
+#include "ftxui/component/component_base.hpp"  // for ComponentBase
+#include "ftxui/dom/elements.hpp"
 
-int main () {
-  cout << endl << "Launched cOS Wallet Manager" << endl;
-  cout << "\n Version 1.0 \n";
-  cout << "THIS IS A NEW TEST TO SEE IF MY CODE AUTO UPDATES";
+
+std::string logo = 
+    R"(        ____   _____  __          __   _ _      _     __  __                                   
+       / __ \ / ____| \ \        / /  | | |    | |   |  \/  |                                  
+   ___| |  | | (___    \ \  /\  / /_ _| | | ___| |_  | \  / | __ _ _ __   __ _  __ _  ___ _ __ 
+  / __| |  | |\___ \    \ \/  \/ / _` | | |/ _ \ __| | |\/| |/ _` | '_ \ / _` |/ _` |/ _ \ '__|
+ | (__| |__| |____) |    \  /\  / (_| | | |  __/ |_  | |  | | (_| | | | | (_| | (_| |  __/ |   
+  \___|\____/|_____/      \/  \/ \__,_|_|_|\___|\__| |_|  |_|\__,_|_| |_|\__,_|\__, |\___|_|   
+                                                                                __/ |          
+                                                                               |___/           
+)";
+
+using namespace ftxui;
+
+
+int main() {
+  auto Buttons = 
+    Container::Vertical({
+          Container::Horizontal({
+                  Button("Create", , ButtonOption::Ascii()),
+                  Button("List", action2, ButtonOption::Ascii()),
+                  Button("Delete", action3, ButtonOption::Ascii()),
+          }),
+      }) | border;
+  Element
+  auto screen = ScreenInteractive::Fullscreen();
+  std::system("clear");
+  screen.Loop(Buttons);
   return 0;
 }
-
